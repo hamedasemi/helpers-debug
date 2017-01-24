5fa5757b65b4af2f00683720bda0c74fb9b0ccb4
Hamed Asemi <h-@live.se>
2017-01-24 12:34:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+
+## Install
+
+```sh
+npm install webrew-helpers-debug --save-dev
+```
+
+## Usage
+
+```javascript
+
+import { DEBUG } from 'webrew-helpers-debug'
+
+DEBUG && console.log('A debug message')
+
+console.log(typeof DEBUG)
+
+```
---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var DEBUG = exports.DEBUG = typeof process.env.DEBUG !== "undefined";
+
+var _module = {
+    DEBUG: DEBUG
+};
+
+exports.default = _module;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJERUJVRyIsInByb2Nlc3MiLCJlbnYiLCJtb2R1bGUiXSwibWFwcGluZ3MiOiI7Ozs7O0FBQU8sSUFBTUEsd0JBQVEsT0FBT0MsUUFBUUMsR0FBUixDQUFZRixLQUFuQixnQkFBZDs7QUFFUCxJQUFJRyxVQUFTO0FBQ1RIO0FBRFMsQ0FBYjs7a0JBSWVHLE8iLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgREVCVUcgPSB0eXBlb2YgcHJvY2Vzcy5lbnYuREVCVUcgIT09IGB1bmRlZmluZWRgXG5cbmxldCBtb2R1bGUgPSB7XG4gICAgREVCVUdcbn1cblxuZXhwb3J0IGRlZmF1bHQgbW9kdWxlIl19
